c0462683e659660a303922a2a9cd7500550d950e
Cryoq <cryoqq@gmail.com>
2024-04-25 01:17:46 -0500

    Added stuff
    Fixed up code and added bullet hell

--- Main.py
+++ Main.py
@@ -1,48 +1,22 @@
 from Constants import *
 import sys
-from abc import ABC, abstractmethod
+from sprites import *
 
-class Hearts:
-    def __init__(self) -> None:
-        self.img = "3hearts.png"
-        self.setHeartSprite()
-        
-    def deleteHeart(self):
-        imageList = list(self.img)
-        number = self.img[0]
-        imageList[0] = str(number-1)
-        self.image = ''.join(imageList)
-        
-    def addHeart(self):
-        imageList = list(self.img)
-        number = self.image[0]
-        imageList[0] = str(number+1)
-        self.image = ''.join(imageList)
-        
-    def setHeartSprite(self):
-        self.image = pygame.image.load(f"sprites/{self.img}")
-        self.image.set_colorkey((0,0,0), RLEACCEL)
-        self.image = pygame.transform.smoothscale(self.image, (150,150))
+def setScore(score,firstRun = False):
+    text = font.render(f"Score: {score}", True, WHITE)
+    if firstRun:
+        rect = text.get_rect()
+        rect.center = (WIDTH-150,73)
+        return text, rect
+    return text
 
-# Basic movement for sprites
-class Movement(pygame.sprite.Sprite, ABC):
-    def __init__(self) -> None:
-        pygame.sprite.Sprite.__init__(self)
-        self.x = WIDTH/2
-        self.y = HEIGHT/2
+def gameOver():
+    gameoverFont = pygame.font.SysFont("Comis Sans MS", 100)
+    gameoverText = gameoverFont.render("GAME OVER!!!", True, WHITE)
+    gameoverRect = gameoverText.get_rect()
+    gameoverRect.center = (WIDTH//2, HEIGHT//2)
+    screen.blit(gameoverText, gameoverRect)
     
-<<<<<<< Updated upstream
-    @property
-    def x(self):
-        return self._x
-    @x.setter
-    def x(self,coord):
-        self._x = coord if coord > 0 and coord < WIDTH  else self._x
-        
-    @property
-    def y(self):
-        return self._y
-=======
 # Part one of the game ( basic wiz, bullets and spider movement )
 def partOne():
     global iterator, prevLives, score
@@ -96,135 +70,28 @@ def partTwo():
 def partThree():
     pass
     
->>>>>>> Stashed changes
     
-    @y.setter
-    def y(self, coord):
-        self._y = coord if coord > 0 and coord < HEIGHT else self._y
-
-    def moveLeft(self,dec):
-        self.x -= dec
-        
-    def moveRight(self, inc):
-        self.x += inc
-    
-    def moveUp(self,dec):
-        self.y -= dec
-        
-    def moveDown(self, inc):
-        self.y += inc
-        
-    def getPosition(self):
-        return self.x, self.y
-    
-    @abstractmethod
-    def update(self):
-        raise NotImplementedError("You need an update function")
-
-class Wizard(Movement):
-    def __init__(self) -> None:
-        self.image = pygame.image.load("sprites/wiz.png").convert()
-        self.image.set_colorkey((0,0,0), RLEACCEL)
-        self.image = pygame.transform.scale(self.image, (200,200))
-        Movement.__init__(self)
-        self.y = HEIGHT - self.image.get_size()[1]
-        self.movingLeft = True
-        self.movingRight = False
-        self.buffer = 0
-        self.speed = 2   
-        
-    def update(self, keyspressed: dict):
-        if keyspressed[K_RIGHT]:
-            self.moveRight(self.speed)
-            if self.movingLeft == True:
-                self.image = pygame.transform.flip(self.image, True, False)
-                self.movingLeft, self.movingRight = False, True
-    
-        if keyspressed[K_LEFT]:
-            self.moveLeft(self.speed)
-            if self.movingRight == True:
-                self.image = pygame.transform.flip(self.image, True, False)
-                self.movingRight, self.movingLeft = False, True
-        
-        if keyspressed[K_SPACE]:
-            if self.buffer == 0:
-                x,y = self.getPosition()
-                if self.movingRight:
-                    x += self.image.get_size()[0]
-                for i in range(50):
-                    bullet = Bullet((x,y-i))
-                    bullets.add(bullet)
-                self.buffer = 150
-            
-        
-        self.buffer = self.buffer - 1 if self.buffer != 0 else self.buffer
-    
-    @property
-    def x(self):
-        return self._x
-
-    @x.setter
-    def x(self,coord):
-        self._x = coord if coord > 0 and coord < WIDTH - self.image.get_size()[0] else self._x
-
-class Spider(Movement):
-    def __init__(self,coord) -> None:
-        Movement.__init__(self)
-        self.y = randint(0,HEIGHT/2)
-        self.image = pygame.image.load("sprites/spider1.png")
-        self.image.set_colorkey((0,0,0),RLEACCEL)
-        self.image = pygame.transform.scale(self.image, (50,50))
-        self.rect = self.image.get_rect()
-        self.rect.center = ((coord))
-        self.buffer = 10
-        self.iterator = 0
-        
-    def update(self):
-        if self.rect.center[0] <= WIDTH:
-            self.rect.move_ip(1,0)
-        else:
-            global lives
-            lives -= 1
-            self.kill()
-    
-    def getSize(self):
-        return self.image.get_size()
-
-class Bullet(Movement):
-    def __init__(self,otherSurf) -> None:
-        Movement.__init__(self)
-        self.image = pygame.image.load("sprites/Bullet.png").convert()
-        self.image = pygame.transform.scale(self.image, (25,25))
-        self.image.set_colorkey((0,0,0), RLEACCEL)
-        self.rect = self.image.get_rect()
-        self.rect.center = (otherSurf)
-        
-    def update(self):
-        if self.rect.center[1] >= 0:
-            self.rect.move_ip(0, -5)
-        else:
-            self.kill()
-
 #-------------Main--------------#
 
+# Initilizing Pygame
 pygame.init()
+pygame.mixer.init()
+
+# Setting up screen
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Wizard Game")
 clock = pygame.time.Clock()
 
-bullets = pygame.sprite.Group()
+# Sets up the enemies and wizard
 enemies = pygame.sprite.Group()
 player = pygame.sprite.Group()
 user = Wizard()
 player.add(user)
 
+# Iteration for spawning spiders
 spawning = 500
 iterator = 0
 
-<<<<<<< Updated upstream
-score = 0
-lives = 3
-prevLives = lives
-=======
 part2 = False
 spiderBullets = pygame.sprite.Group()
 spiderBulletAngle = 0
@@ -233,62 +100,34 @@ lastSpiderBullet = pygame.time.get_ticks()
 part3 = False
 
 # Hearts
-score = 5
+score = 4
 prevLives = Wizard.lives
->>>>>>> Stashed changes
 hearts = Hearts()
 
-font = pygame.font.SysFont("Comic Sans MS", 32)
+# Sets Font
+font = pygame.font.SysFont("ebrima", 32,bold=True)
 
-scoreText = font.render(f"Score: {score}", True, BLACK, WHITE)
-scoreRect = scoreText.get_rect()
-scoreRect.center = (75,25)
+# Sets up the scoring and the border image
+scoreText,scoreRect = setScore(score,True)
+scoreBorder = pygame.image.load("sprites/Border.png").convert_alpha()
+scoreBorder.set_colorkey((0,0,0), RLEACCEL)
+scoreBorder = pygame.transform.scale(scoreBorder, (scoreBorder.get_size()[0] * 5,scoreBorder.get_size()[1] * 5))
 
-livesText = font.render(f"Lives: {lives}", True, BLACK, WHITE)
-livesRect = livesText.get_rect()
-livesRect.center = (WIDTH-75, 25)
-
-gameoverFont = pygame.font.SysFont("Comis Sans MS", 100)
-gameoverText = gameoverFont.render("GAME OVER!!!", True, BLACK, WHITE)
-gameoverRect = gameoverText.get_rect()
-gameoverRect.center = (WIDTH//2, HEIGHT//2)
+# Loads the background
+bg = pygame.image.load("sprites/bg.png")
+bg = pygame.transform.smoothscale(bg, (WIDTH,HEIGHT))
 
 running = True
 while running:
     clock.tick(144)
+    screen.fill(WHITE)
+    screen.blit(bg,(0,0))
+    
+    # Lets you press x to exit game
     for event in pygame.event.get():
         if event.type == QUIT: 
             pygame.quit()
             sys.exit()
-<<<<<<< Updated upstream
-    if lives != 0:
-        if iterator >= spawning:
-            spiders = Spider((0, randint(50,HEIGHT-300)))
-            enemies.add(spiders)
-            iterator = 0
-        iterator += 1
-                
-        pressedKeys = pygame.key.get_pressed()
-        user.update(pressedKeys)
-        screen.fill(WHITE)
-        screen.blit(user.image, user.getPosition())
-        bullets.draw(screen)
-        bullets.update()
-        enemies.draw(screen)
-        enemies.update()
-        screen.blit(scoreText, scoreRect)
-        screen.blit(livesText, livesRect)
-        if pygame.sprite.groupcollide(bullets, enemies, True, True):
-            score += 1
-            scoreText = font.render(f"Score: {score}", True, BLACK, WHITE)
-        if prevLives != lives:
-            livesText = font.render(f"Lives: {lives}", True, BLACK, WHITE)
-            prevLives = lives
-    else:
-        screen.blit(scoreText, scoreRect)
-        screen.blit(livesText, livesRect)
-        screen.blit(gameoverText, gameoverRect)
-=======
             
     if Wizard.lives > 0 and BulletHellPlayer.lives > 0:
 
@@ -377,5 +216,4 @@ while running:
     
     screen.blit(hearts.image, (25,15))
 
->>>>>>> Stashed changes
     pygame.display.flip()
